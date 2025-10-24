328f2f3d31521cc997ad33f15f0afa4906d095f8
Davis Bryan <davisbryan595@gmail.com>
2025-10-23 17:32:15 -0700

    added floating button

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
+import FloatingCallButton from "@/components/FloatingCallButton"
 
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
