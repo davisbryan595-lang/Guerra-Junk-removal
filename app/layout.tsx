cc4d8929481bcf414412f72e2a9bfc8842fdf9e0
Davis Bryan <davisbryan595@gmail.com>
2025-10-23 17:34:40 -0700

    floating button

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,18 @@ import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import FloatingCallButton from "@/components/FloatingCallButton"
 
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <FloatingCallButton /> {/* 👈 Add this line */}
+      </body>
+    </html>
+  )
+}
+
+
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
